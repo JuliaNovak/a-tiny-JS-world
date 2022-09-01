71b4b50b264f460d1b9ab29c278f4226a8f34b1b
JuliaNovak <julia.novak9@gmail.com>
2022-09-01 14:01:32 +0200

    add tiny-js-world project

--- index.js
+++ index.js
@@ -7,9 +7,59 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+   species: "dog",
+   name: "Toby",
+   gender: "male",
+   legs: 4,
+   hands: 0,
+   saying: "woof-woof!",
+};
 
+const cat = {
+   species: "cat",
+   name: "Lily",
+   gender: "female",
+   legs: 4,
+   hands: 0,
+   saying: "meow!",
+};
+
+const woman = {
+   species: "human",
+   name: "Linda",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: "What a pretty kitten!",
+};
+
+const man = {
+   species: "human",
+   name: "Jim",
+   gender: "male",
+   legs: 2,
+   hands: 2,
+   saying: "Heyyo!",
+};
+
+const catWoman = {
+   species: "cat",
+   name: "Ro",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+};
 
 // ======== OUTPUT ========
+
+const inhabitantsArr = [dog, cat, man, woman, catWoman];
+inhabitantsArr.forEach((inhabitant) => {
+   const inhabitantToArr = Object.values(inhabitant);
+   print(inhabitantToArr.map((prop) => prop));
+});
+
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +77,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
